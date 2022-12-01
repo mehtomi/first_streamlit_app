b4ba07fa206e4050ccaab73d4a53aff281456885
mehtomi <119599891+mehtomi@users.noreply.github.com>
2022-12-01 15:50:46 +0200

    Update streamlit_app.py
    Lisätty emojit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.text('Kauramaitoa, kiitos olen vegaaniapina')
+streamlit.text('🥑🥑🥑 Kauramaitoa, kiitos olen vegaaniapina 🥑🥑🥑')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🍞 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
