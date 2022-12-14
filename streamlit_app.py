dd2b972b809866a05c9a41bfa8b8aa70eddf2118
mehtomi <119599891+mehtomi@users.noreply.github.com>
2022-12-14 15:27:07 +0200

    Update streamlit_app.py
    lisätty snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # tehdään siitä joku nätti taulukko
 streamlit.dataframe(fruityvice_normalized)
+
+import.snowflake.connector
