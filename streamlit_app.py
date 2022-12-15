7df2352eee74b966e71368c93a2b1fe6fd9b7d1f
mehtomi <119599891+mehtomi@users.noreply.github.com>
2022-12-15 15:08:11 +0200

    Update streamlit_app.py
    lisättiin painike joka hakee funktiolla hedelmälistan

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,12 +52,19 @@ streamlit.write('The user entered ', fruit_choice)
 #katkaistaan koodi hetkeksi tähän
 streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
+
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#Snowfale related functions
+def get_fruit_load_list():
+      with my_cnx.cursor() as my_cur:
+      my_cur.execute("select * from FRUIT_LOAD_LIST")
+      return my_cur.fetchall()
+
+#Add a button to load fruit load list
+if streamlit.button('Get Fruit Load List'):
+      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      my_data_rows = get_fruit_load_list()
+      streamlit.dataframe(my_data_rows)
 
 # Käyttäjä voi kirjoittaa toisen hedelmän
 # streamlit.header('What fruit would you like to add?')
