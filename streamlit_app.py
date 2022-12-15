55c38f582c2bebc9b2b89fcd37e583b763259173
mehtomi <119599891+mehtomi@users.noreply.github.com>
2022-12-15 14:07:41 +0200

    Update streamlit_app.py
    tehtiin if-else rakenne

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,16 +28,20 @@ streamlit.dataframe(fruits_to_show)
 
 # Käyttäjä voi kirjoittaa hedelmän
 streamlit.header('Fruityvice Fruit Advice')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choise:
+      streamlit.error("Please help to select a fruit to get information.")
+  else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      # normalisoidaan json data
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      # tehdään siitä joku nätti taulukko
+      streamlit.dataframe(fruityvice_normalized)
+expect URLError as e:
+      streamlit.error()
+
 streamlit.write('The user entered ', fruit_choice)
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# normalisoidaan json data
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# tehdään siitä joku nätti taulukko
-streamlit.dataframe(fruityvice_normalized)
 
 #katkaistaan koodi hetkeksi tähän
 streamlit.stop()
